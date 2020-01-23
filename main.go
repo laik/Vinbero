aa3d17236cc8babe2cb890eec679e206e7938388
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2020-01-24 04:42:12 +0900

    fmt main.go

--- main.go
+++ main.go
@@ -2,46 +2,47 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
- 	"syscall"
-	"fmt"
-//	"time"
+	"syscall"
+
+	//	"time"
 	"net"
 
 	"github.com/pkg/errors"
+	"github.com/takehaya/srv6-gtp/srv6"
 	"github.com/takehaya/srv6-gtp/version"
 	"github.com/takehaya/srv6-gtp/xdptool"
-	"github.com/takehaya/srv6-gtp/srv6"
 
 	"gopkg.in/yaml.v2"
 )
 
-type FunctionsPram struct{
+type FunctionsPram struct {
 	Action string `yaml:"action,omitempty"`
-	Addr string `yaml:"addr,omitempty"`
+	Addr   string `yaml:"addr,omitempty"`
 }
 
-type Transitv4Pram struct{
-	Action string `yaml:"action,omitempty"`
-	Addr string `yaml:"addr,omitempty"`
-	SAddr string `yaml:"action_source_addr,omitempty"`
+type Transitv4Pram struct {
+	Action   string   `yaml:"action,omitempty"`
+	Addr     string   `yaml:"addr,omitempty"`
+	SAddr    string   `yaml:"action_source_addr,omitempty"`
 	Segments []string `yaml:"segments,omitempty"`
 }
 
-type SetPram struct{
+type SetPram struct {
 	Functions []FunctionsPram `yaml:"functions,omitempty"`
 	Transitv4 []Transitv4Pram `yaml:"transitv4,omitempty"`
 }
 
 type Config struct {
-	ElfFilepath string `yaml:"elffilepath,omitempty"`
-	ProgName    string `yaml:"progname,omitempty"`
+	ElfFilepath string   `yaml:"elffilepath,omitempty"`
+	ProgName    string   `yaml:"progname,omitempty"`
 	Devices     []string `yaml:"devices,omitempty"`
-	Set         SetPram `yaml:"set,omitempty"`
-} 
+	Set         SetPram  `yaml:"set,omitempty"`
+}
 
 func ConfigReadOnStruct(fileBuffer []byte) (Config, error) {
 	data := Config{}
@@ -61,7 +62,6 @@ func main() {
 
 type ipAddressList []string
 
-
 var iface = flag.String("iface", "eth0", "Interface to bind XDP program to")
 var elf = flag.String("elf", "out/srv6.o", "clang/llvm compiled binary file")
 var ipList = ipAddressList{"10.0.0.1"}
@@ -75,7 +75,7 @@ func run() error {
 		log.Printf("version: %s.%s\n", version.Version, version.Revision)
 		return nil
 	}
-	
+
 	// config loading
 	buf, err := ioutil.ReadFile(*configFile)
 	if err != nil {
@@ -94,14 +94,14 @@ func run() error {
 		return errors.WithStack(err)
 	}
 
-	for _, dev := range data.Devices{
+	for _, dev := range data.Devices {
 		// Attach to interface
 		err = xdptool.Attach(coll, data.ProgName, dev)
 		if err != nil {
 			return errors.WithStack(err)
 		}
-		log.Println("attached device: %v", dev)
-	}	
+		log.Printf("attached device: %v", dev)
+	}
 
 	// match map
 	function_m, err := srv6.NewFunctionTable(coll)
@@ -121,9 +121,8 @@ func run() error {
 		return errors.WithStack(err)
 	}
 
-
 	// write to redirect map
-	for _, dev := range data.Devices{
+	for _, dev := range data.Devices {
 		iface, err := net.InterfaceByName(dev)
 		if err != nil {
 			log.Printf("fail to interface: %v", dev)
@@ -135,17 +134,17 @@ func run() error {
 			return errors.WithStack(err)
 		}
 	}
-	
+
 	// write to params map
-	if funcs:= data.Set.Functions; 0 < len(funcs) {
-		for _, fn := range funcs{
+	if funcs := data.Set.Functions; 0 < len(funcs) {
+		for _, fn := range funcs {
 			action := fn.Action
 			addr := fn.Addr
 			fn_enum := srv6.SEG6LocalActionEnum(action)
-			if srv6.SEG6_LOCAL_ACTION_MAX == fn_enum{
+			if srv6.SEG6_LOCAL_ACTION_MAX == fn_enum {
 				return errors.New(fmt.Sprintf("%v not found", action))
 			}
-			if addr == ""{
+			if addr == "" {
 				return errors.New(fmt.Sprintf("addr not found"))
 			}
 
@@ -155,23 +154,23 @@ func run() error {
 			// for index, bit := range big_convaddr{
 			// 	litle_convaddr[16 - 1 - index] = bit
 			// }
-			
+
 			var convip [16]byte
 			copy(convip[:], net.ParseIP(addr).To16())
 			fmt.Println(convip)
 			err := function_m.Update(
-				srv6.FunctionTable{Function: fn_enum}, 
+				srv6.FunctionTable{Function: fn_enum},
 				convip,
 			)
 			if err != nil {
 				log.Printf("Unable to Insert into eBPF map: %v", err)
 				return errors.WithStack(err)
 			}
-			log.Println("Insert into eBPF map seg6 action: %v", fn)
+			log.Printf("Insert into eBPF map seg6 action: %v", fn)
 		}
 	}
 	if tranv4s := data.Set.Transitv4; 0 < len(tranv4s) {
-		for _, tran := range tranv4s{
+		for _, tran := range tranv4s {
 			action := tran.Action
 			addr := tran.Addr
 			saddr := tran.SAddr
@@ -179,15 +178,15 @@ func run() error {
 
 			encap_enum := srv6.SEG6EncapModeInt(action)
 			log.Println("encap_enum", encap_enum)
-			if srv6.SEG6_IPTUN_MODE_MAX == encap_enum{
+			if srv6.SEG6_IPTUN_MODE_MAX == encap_enum {
 				return errors.New(fmt.Sprintf("%v not found", action))
 			}
-			if addr == ""{
+			if addr == "" {
 				return errors.New(fmt.Sprintf("addr not found"))
 			}
 
 			var newsegments [srv6.MAX_SEGMENTS][16]byte
-			for i, seg := range segments{
+			for i, seg := range segments {
 				var segmentaddr [16]byte
 				copy(segmentaddr[:], net.ParseIP(seg).To16())
 				log.Println("segments addr", i, segmentaddr)
@@ -206,12 +205,12 @@ func run() error {
 
 			err := v4table_m.Update(
 				srv6.TransitTablev4{
-					Action: encap_enum,
+					Action:         encap_enum,
 					Segment_length: uint32(l),
-					Saddr: convSaddr,
-					Segments: newsegments,
-					}, 
-					convAddr,
+					Saddr:          convSaddr,
+					Segments:       newsegments,
+				},
+				convAddr,
 			)
 			if err != nil {
 				log.Printf("Unable to Insert into eBPF map: %v", err)
@@ -220,10 +219,8 @@ func run() error {
 		}
 	}
 
-
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
 
 	log.Println("XDP program successfully loaded and attached. Counters refreshed every second.")
 	log.Println("Press CTRL+C to stop.")
@@ -232,30 +229,28 @@ func run() error {
 	for {
 		select {
 		// case <-ticker.C:
-			// fmt.Println("IP DROPs")
-			// for i := 0; i < len(ipList); i++ {
-			// 	value, err := ma.Get(uint32(i))
-			// 	if err != nil {
-			// 		fmt.Printf("LookupInt failed: %v", err)
-			// 	}
-			// 	fmt.Printf("%18s    %d\n", ipList[i], value)
-			// }
-			// fmt.Println()
+		// fmt.Println("IP DROPs")
+		// for i := 0; i < len(ipList); i++ {
+		// 	value, err := ma.Get(uint32(i))
+		// 	if err != nil {
+		// 		fmt.Printf("LookupInt failed: %v", err)
+		// 	}
+		// 	fmt.Printf("%18s    %d\n", ipList[i], value)
+		// }
+		// fmt.Println()
 		case <-signalChan:
 			// TODO: change prepare priority elf->ebpfmap make -> xdp attach
-			for _, dev := range data.Devices{
+			for _, dev := range data.Devices {
 				// Attach to interface
 				err = xdptool.Detach(dev)
 				if err != nil {
 					return errors.WithStack(err)
 				}
-				log.Println("attached device: %v", dev)
+				log.Printf("attached device: %v", dev)
 			}
 			fmt.Println("\nDetaching program and exit")
 
 			return nil
 		}
 	}
-	
-	return nil
 }
