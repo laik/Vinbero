5b8b004643fb711ed4f7f2b72a56fd53241fb760
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2020-01-23 09:59:22 +0900

    Fix build_elf.sh

--- build_elf.sh
+++ build_elf.sh
@@ -8,6 +8,6 @@ cp ./Makefile-bpf $prog_dir/Makefile
 cp -r ./src $prog_dir
 cp -r ./include $prog_dir
 pushd $prog_dir
-bear make EXTRA_CFLAGS="$*"
+make EXTRA_CFLAGS="$*"
 popd
 cp $prog_dir/src/srv6.o ./out/
