e098cfd0d1a77abd32a9c749e13d0229d8cf5e53
Takeru Hayasaka <hayatake396@gmail.com>
2021-01-25 17:08:56 +0900

    add graph save and fix graph legend

--- docs/performance/graph/dx4_graph.py
+++ docs/performance/graph/dx4_graph.py
@@ -4,6 +4,7 @@ import numpy as np
 sns.set()
 
 name = "End.DX4"
+formatimage = "pdf"
 vinbero_single = [3.9845887999999996, 1.9922943999999998, 2.87834112, 3.29252864, 3.5913728, 3.4723181878651688]
 vinbero_multi = [3.9007027200000004, 1.9922943999999998, 2.9569843199999997, 3.34495744,3.6438016, 3.4402718202247193]
 linux_single = [1.7496473600000002, 0.85302272, 1.30023424, 1.51781376,  1.62922496, 1.6607558759550562]
@@ -21,9 +22,9 @@ plt.bar(left, vinbero_single, color='r', width=width, align='center', label="vin
 plt.bar(left+width, linux_single, color='b', width=width, align='center', label="linux")
 
 plt.xticks(left + width/2, labels)
-plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0, fontsize=18)
+plt.legend(loc="best")
 
-plt.show()
+plt.savefig(f"./images/dx4_single_flow.{formatimage}")
 
 
 left = np.arange(len(vinbero_multi))
@@ -35,5 +36,4 @@ plt.bar(left, vinbero_multi, color='r', width=width, align='center', label="vinb
 plt.bar(left+width, linux_multi, color='b', width=width, align='center', label="linux")
 
 plt.xticks(left + width/2, labels)
-plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0, fontsize=18)
-plt.show()
+plt.savefig(f"./images/dx4_multi_flow.{formatimage}")
