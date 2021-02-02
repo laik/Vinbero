f9bd464f5b2fdee31a01ef66711d1ee074cf7331
Takeru Hayasaka <hayatake396@gmail.com>
2021-02-02 18:34:53 +0900

    build images

--- docs/performance/graph/mobile_v4_graph.py
+++ docs/performance/graph/mobile_v4_graph.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 sns.set()
 
+
+formatimage = "pdf"
+name = "GTP-U/SRv6"
 hmgtp4d_single = [5.4525952, 3.69098752, 3.19291392, 2.95174144, 2.8272230400000002, 2.8181952719101124]
 hmgtp4d_multi = [67.80092416, 34.07872, 16.908288, 8.50132992, 4.25066496, 3.238568212134831]
 endmgtp4e_single = [5.24288, 3.1981568, 3.44981504, 3.67263744, 3.78667008, 3.624067164044944]
@@ -12,7 +15,7 @@ left = np.arange(len(hmgtp4d_single))
 labels = [64, 128, 256, 512, 1024, 1424]
 width = 0.3
 
-plt.title("Payload pps (single flow)")
+plt.title(f"{name}/Payload pps (single flow)")
 plt.ylabel("Mpps")
 plt.xlabel("byte")
 
@@ -20,10 +23,8 @@ plt.bar(left, hmgtp4d_single, color='r', width=width, align='center', label="H.M
 plt.bar(left+width, endmgtp4e_single, color='b', width=width, align='center', label="End.M.GTP4.E")
 
 plt.xticks(left + width/2, labels)
-plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0, fontsize=18)
-
-plt.show()
-
+plt.legend(loc="best")
+plt.savefig(f"./images/mobile_v4_single_flow.{formatimage}")
 
 left = np.arange(len(hmgtp4d_multi))
 plt.title("Payload pps (multi flow)")
@@ -34,5 +35,7 @@ plt.bar(left, hmgtp4d_multi, color='r', width=width, align='center', label="H.M.
 plt.bar(left+width, endmgtp4e_multi, color='b', width=width, align='center', label="End.M.GTP4.E")
 
 plt.xticks(left + width/2, labels)
-plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0, fontsize=18)
-plt.show()
+# plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0, fontsize=18)
+# plt.show()
+
+plt.savefig(f"./images/mobile_v4_multi_flow.{formatimage}")
